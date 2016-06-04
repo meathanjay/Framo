7c7ffba5dffa2ff3b1e02cf76290dd4255021ba5
Meathanjay Baidya <mb@Meathanjays-MacBook-Pro.local>
2016-06-05 00:33:39 +0600

    Front Controller Pattern

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-$input = $_GET['name'];
-printf('Hello %s', $input);
+require_once __DIR__.'/init.php';
+
+$input = $request->get('name' , 'Default Name');
+
+$response->setContent(sprintf("Hello %s",htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
