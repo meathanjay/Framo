048975ca893c9bc7be912605949f6fe8bb46fa18
Meathanjay Baidya <baidya.me@gmail.com>
2016-11-05 23:59:46 +0600

    pages deleted

--- src/pages/hello.php
+++
@@ -1,2 +0,0 @@
-<title><?php echo $title; ?></title>
-Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- src/pages/index.php
+++
@@ -1,1 +0,0 @@
-Hello <?php echo htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
